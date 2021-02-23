a65de2c7ac7ab64f417068273a8c97d51d699bf7
ismael.ramos <ismael.ramos@meta.es>
2021-02-23 23:37:46 +0100

    fix config

--- scripts/upload-sourcemaps.sh
+++ scripts/upload-sourcemaps.sh
@@ -1,12 +1,12 @@
 # Assumes you're in a git repository
 export ORG=metastartup
 export PROJECT=sentry-demo
-export $VERSION=0.0.1
+export VERSION=0.0.1
 
-sentry-cli releases --org $ORG new --project $PROJECT $VERSION
+sentry-cli releases --org "$ORG" new --project "$PROJECT" "$VERSION"
 
-sentry-cli releases set-commits --auto $VERSION
+sentry-cli releases --org "$ORG" --project "$PROJECT" set-commits --auto "$VERSION"
 
-sentry-cli releases --org metastartup --project sentry-demo files 0.0.1 upload-sourcemaps ./dist/my-dream-app/ --rewrite --strip-common-prefix
+sentry-cli releases --org "$ORG" --project "$PROJECT" files "$VERSION" upload-sourcemaps ./dist/"$PROJECT"/ --rewrite --strip-common-prefix
 
-sentry-cli releases finalize $VERSION
+sentry-cli releases --org "$ORG" --project "$PROJECT" finalize "$VERSION"
