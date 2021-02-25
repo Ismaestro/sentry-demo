635f74500806f0aa77990a77e31a288767c37d91
ismael.ramos <ismael.ramos@meta.es>
2021-02-25 20:10:48 +0100

    new release 0.0.10

--- scripts/upload-sourcemaps.sh
+++ scripts/upload-sourcemaps.sh
@@ -8,15 +8,13 @@ REPOSITORY_URL=git@github.com:Ismaestro/sentry-demo.git
 
 export SENTRY_ORG=metastartup
 export SENTRY_PROJECT=sentry-demo
-export SENTRY_LOG_LEVEL=
 
-npm run build:prod
+#npm run build:prod
 
 sentry-cli releases new "$VERSION"
 
 sentry-cli releases set-commits --auto "$VERSION"
 
-sentry-cli releases files "$VERSION" upload-sourcemaps ./dist/"$PROJECT"/ \
-    --url-prefix "~/$SENTRY_PROJECT" --strip-common-prefix
+sentry-cli releases files "$VERSION" upload-sourcemaps ./dist/"$PROJECT"/ --strip-common-prefix
 
 sentry-cli releases finalize "$VERSION"
